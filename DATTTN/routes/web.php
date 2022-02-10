d738bf60a3da3462c3b47cc57b6346567e153434
Thao Nguyen <55941949+p-o302@users.noreply.github.com>
2022-02-10 08:39:49 +0700

    change local

--- DATTTN/routes/web.php
+++ DATTTN/routes/web.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     // return view('layout');
+//     echo "abc";
+// });
+
+Route::get('/', function(){
     return view('layout');
 });
 
